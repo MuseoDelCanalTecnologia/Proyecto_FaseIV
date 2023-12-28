3b9a306756cc14b648df572afaa85e7bda5292f6
MuseoDelCanalTecnologia <museodelcanaltecnologia@gmail.com>
2023-12-28 16:58:29 -0500

    agregue en ampliacion componentes videos ingles español

--- src/app/ampliacion/canal-rutas/canal-rutas.component.ts
+++ src/app/ampliacion/canal-rutas/canal-rutas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocketService } from 'src/app/services/socket.service';
 
 @Component({
   selector: 'app-canal-rutas',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./canal-rutas.component.css']
 })
 export class CanalRutasComponent {
+  data: any;
+  constructor(private wsSocket: SocketService, private router: Router) {}
+  videoES: boolean = false;
+  videoEN: boolean = false;
 
+  ngOnInit(): void {
+    this.wsSocket.homeEscuchar().subscribe((data: any) => {
+      this.router.navigateByUrl(data);
+      console.log(data);
+    });
+    this.recibiendoPortal();
+    this.idioma();
+  }
+
+  recibiendoPortal() {
+    this.wsSocket.escuchandoPortal().subscribe((data) => {
+      this.data = data;
+      this.router.navigateByUrl(this.data);
+    });
+  }
+
+  idioma() {
+    this.wsSocket.idiomaGet().subscribe((data) => {
+      
+      if (data === 'es') {
+        this.videoES = true;
+        this.videoEN = false;
+      } else {
+        this.videoEN = true;
+        this.videoES = false;
+      }
+    });
+  }
 }
